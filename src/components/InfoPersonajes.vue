5bd291c9821a32cfba6a94be37195f0a5b19f77b
Paola <paolasalazar90@gmail.com>
2020-10-20 18:53:41 -0300

    better

--- src/components/InfoPersonajes.vue
+++ src/components/InfoPersonajes.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <div class="fondo" v-for="(things,index) in characters" :key="index">
-      <img :src="things.image"><span>{{things.name}}</span>
+  <div class="fondo">
+    <div  v-for="(things,index) in characters" :key="index">
+      <img :src="things.image">
+      <h2>{{things.name}}</h2>
     </div>
   </div>
 </template>
@@ -20,22 +21,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+template{
+  margin: 0;
+}
 .fondo{
   background-image: url('https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png');
   background-size: cover;
   background-position: center;
-  margin: 0;
 }
 img{
   border-radius: 50%;
   margin-top: 2em;
   margin-left: 2em;
   margin-right: 2em;
-  height: 5em;
+  height: 10em;
 }
-span{
+h2{
   color: green;
   font-size: 2em;
   font-weight: bold;
+  display: inline;
 }
 </style>
